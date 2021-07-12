e11e602e5e02f4b057b998307a2b290e3d567126
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-12 11:59:04 +0900

    ブログモデルのマイグレーションファイルの編集

--- db/migrate/20210712024726_create_blogs.rb
+++ db/migrate/20210712024726_create_blogs.rb
@@ -1,7 +1,10 @@
 class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
-
+      t.string :title,      null: false
+      t.string :catch_copy, null: false
+      t.text :article,      null: false
+      t.references :user,   null: false, foreign_key: true
       t.timestamps
     end
   end
