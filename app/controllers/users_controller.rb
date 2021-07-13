5b897189fb962262ba32830433daba427aa42e2f
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-13 21:10:02 +0900

    ユーザー詳細ページの実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:show]
+
+    def show
+        @blogs = @user.blogs.with_attached_image.order("created_at DESC")
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+end
