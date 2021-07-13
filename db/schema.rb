84ee0ba9efc4d85cd680797cb12e37079ce7a3b0
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-13 16:05:22 +0900

    外部キーの追加とファイルの順番を変更

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2021_07_13_051312) do
     t.string "title", null: false
     t.string "catch_copy", null: false
     t.text "article", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 2021_07_13_051312) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "blogs", "users"
 end
