629c165917c018c25f1a8f3bd1e352770ffa9df2
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-13 16:27:16 +0900

    記事投稿機能の実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,24 @@
 class BlogsController < ApplicationController
     def index
+        @blogs = Blog.includes(:user).with_attached_image.order('created_at DESC')
+    end
+
+    def new
+        @blog = Blog.new
+    end
+
+    def create
+        @blog = Blog.new(blog_params)
+        if @blog.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def blog_params
+        params.require(:blog).permit(:title, :catch_copy, :article, :image).merge(user_id: current_user.id)
     end
 end
