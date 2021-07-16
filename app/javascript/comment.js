043f520eb287528b9f040c90ef778fa344846236
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-16 15:27:42 +0900

    jsからのリクエスト送信

---
+++ app/javascript/comment.js
@@ -0,0 +1,15 @@
+function post (){
+    const submit = document.getElementById("submit");
+    submit.addEventListener('click', (e) =>{
+        e.preventDefault();
+        const form = document.getElementById("form");
+        const blog = document.getElementById("blog");
+        const blogId = blog.getAttribute("value");
+        const formData = new FormData(form);
+        const XHR = new XMLHttpRequest();
+        XHR.open("POST", `/blogs/${blogId}/comments`, true);
+        XHR.responseType = "json";
+        XHR.send(formData)
+    })
+};
+window.addEventListener('load', post)
