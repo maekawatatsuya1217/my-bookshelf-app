aa483520b7101169f3dd3bc08eaa89f8231dc2d9
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-16 10:33:53 +0900

    コントローラーの編集

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -21,6 +21,8 @@ class BlogsController < ApplicationController
     end
 
     def show
+        @comment = comment.new
+        @comments = @blog.comments.includes(:user).order(id: "DESC")
     end
 
     def edit
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+    def create
+        @comment = Comment.new(comment_params)
+        if @comment.save
+          redirect_to blog_path(@comment.blog)
+        else
+          @blog = @comment.blog
+          @comments = @blog.comments.includes(:user).order("created_at DESC")
+          render 'blogs/show'
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:text).merge(user_id: :current_user.user_id, blog_id: params[:blog_id])
+    end
 end
