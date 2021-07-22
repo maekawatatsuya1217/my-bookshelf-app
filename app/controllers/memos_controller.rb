e1a8d2d32c73a58653a9b89bade508a62e1493df
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-22 17:02:38 +0900

    新規投稿機能の実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,8 +1,28 @@
 class MemosController < ApplicationController
 
-    before_action :authenticate_user!, only: :index
+    before_action :authenticate_user!, only: [:index, :new, :create]
 
     def index
         @memos = Memo.where(user_id: current_user.id).includes(:user).order('created_at DESC')
     end
+
+    def new
+        @memo = Memo.new
+    end
+
+    def create
+        @memo = Memo.new(memo_params)
+        if @memo.save
+            redirect_to memos_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def memo_params
+        params.require(:memo).permit(:name, :point, :text).merge(user_id: current_user.id)
+    end
+
 end
