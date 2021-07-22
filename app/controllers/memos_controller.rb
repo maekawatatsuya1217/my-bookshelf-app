6ae05294bcc345e026386ca6f8c9ea0c6ac4b5b2
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-22 09:47:01 +0900

    コントローラーの記述変更

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,7 @@
 class MemosController < ApplicationController
+
+    before_action :authenticate_user!, only: :index
     def index
-        @memos = MEMO.includes(:user).order('created_at DESC')
+        @memos = MEMO.where(user_id: current_user.id).includes(:user).order('created_at DESC')
     end
 end
