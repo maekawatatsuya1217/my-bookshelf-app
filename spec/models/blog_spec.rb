8a265d8c2bc113bc7eb0a2b468e5866297be03a1
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-27 15:49:33 +0900

    テストコードを修正

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -15,27 +15,27 @@ RSpec.describe Blog, type: :model do
       it 'titleが空だと投稿できない' do
         @blog.title = ''
         @blog.valid?
-        expect(@blog.errors.full_messages).to include("Title can't be blank")
+        expect(@blog.errors.full_messages).to include('タイトルを入力してください')
       end
       it 'catch_copyが空だと投稿できない' do
         @blog.catch_copy = ''
         @blog.valid?
-        expect(@blog.errors.full_messages).to include("Catch copy can't be blank")
+        expect(@blog.errors.full_messages).to include('キャッチコピーを入力してください')
       end
       it 'articleが空だと投稿できない' do
         @blog.article = ''
         @blog.valid?
-        expect(@blog.errors.full_messages).to include("Article can't be blank")
+        expect(@blog.errors.full_messages).to include('記事を入力してください')
       end
       it 'categoryが空だと投稿できない' do
         @blog.category_id = 1
         @blog.valid?
-        expect(@blog.errors.full_messages).to include("Category can't be blank")
+        expect(@blog.errors.full_messages).to include('カテゴリーを選択してください')
       end
       it 'imageが空だと投稿できない' do
         @blog.image = nil
         @blog.valid?
-        expect(@blog.errors.full_messages).to include("Image can't be blank")
+        expect(@blog.errors.full_messages).to include('画像を入力してください')
       end
     end
   end
